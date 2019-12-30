fd70648fefc00abf966a899cf6aa1eb6fb3e2356
hariharanragothaman <hariharanragothaman@gmail.com>
2019-12-30 17:28:30 -0500

    Adding function to read content from stdin

--- shell.c
+++ shell.c
@@ -42,6 +42,46 @@
  *  5. Executing system commands and libraries ---- by forking a child and calling execvp.
  */
 
+
+/*
+ *  ------------------------------------------------------------------------------------------------------------
+ */
+
+# define SH_BUF_SIZE 1024
+# define TOK_BUF_SIZE 64
+# define TOK_DELIM " \t\r\n\a"
+
+
+
+/*
+ *  ------------------------------------------------------------------------------------------------------------
+ */
+
+// Understanding the logic here, but need to understand the definitions.....
+
+
+char **split_line(char *line)
+{
+	int buffer_size = TOK_BUF_SIZE, position = 0;
+	char **tokens = malloc(buffer_size * sizeof(char*));
+	char *token, **tokens_backup;
+
+	if (!tokens)
+	{
+		fprintf(stderr, "sh: Allocation error\n");
+		exit(EXIT_FAILURE);
+	}
+
+
+}
+
+
+
+
+
+/*
+ * read_line() - reads entries from stdin
+ */
 char *read_line(void)
 {
 	int buffer_size = SH_BUF_SIZE; 
@@ -54,16 +94,52 @@ char *read_line(void)
 		exit(EXIT_FAILURE);
 	}
 
+	int c; 
+
+	while(1)
+	{
+		c = getchar();
+		if (c == EOF)
+		{
+			exit(EXIT_SUCCESS);
+		}
+
+		else if (c == '\n')
+		{
+			buffer[position] = '\0';
+			return buffer;
+		}
+
+		else
+		{
+			buffer[position] = c; 
+		}
+
+		position++;
+	}
+
+	/* What happens if we have exceeded the buffer??
+	 * We will have to reallocate some memory right?
+	 */
+
+	if (position >= buffsize)
+	{
+		buffer_size += SH_BUF_SIZE;
+		buffer = realloc(buffer, buffer_size);
+
+		// every time you reallocate memory; check for NULL?!
+		if (!buffer)
+		{
+			fprintf(stderr, "sh: Allocation error\n");
+			exit(EXIT_FAILURE);
+		}
+	}
 }
 
 
-
-
-
-
-
-
-
+/*
+ *  run_shell_loop() function - Serves as an orchestrator
+ */
 
 void run_shell_loop(void)
 {
